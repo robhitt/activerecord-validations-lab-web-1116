e0c3eaf0b1f430e735cdb5154a9aa9207085fe3a
Rob Hitt <robhitt@gmail.com>
2017-01-01 18:59:26 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Must choose Fiction or Non-Fiction, %{value} is not a valid category" }
+  validate :title_sticky
+
+  def title_sticky
+    unless !title.nil? && (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top [number]") || title.include?("Guess"))
+      errors.add(:title, "Title needs to be more clickybaity and sticky")
+    end
+  end
+
 end
